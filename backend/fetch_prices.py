8085435b9390832ac001d643d7de8eaedf3e2223
İsmail Burak Azman <116027766+BrkAzmn@users.noreply.github.com>
2025-03-02 10:56:54 +0300

    Update fetch_prices.py

--- backend/fetch_prices.py
+++ backend/fetch_prices.py
@@ -1,7 +1,6 @@
 import requests
 
 def get_gold_price(year, investment):
-    # Örnek API entegrasyonu, API bulunamazsa manuel veri kullanılır
     prices = {2020: (1500, 1900), 2021: (1900, 1800)}  # (Başlangıç, Bitiş)
     price_start, price_end = prices.get(year, (0, 0))
 
